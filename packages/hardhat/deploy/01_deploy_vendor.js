c7f35ec10b49a29d0534f548c788bcb133a37949
Olamigoke John <olamaigokejonny@gmail.com>
2022-04-02 10:21:49 -1200

    second challenge

--- packages/hardhat/deploy/01_deploy_vendor.js
+++ packages/hardhat/deploy/01_deploy_vendor.js
@@ -1,4 +1,4 @@
-// deploy/01_deploy_vendor.js
+//deploy/01_deploy_vendor.js
 
 const { ethers } = require("hardhat");
 
@@ -11,35 +11,35 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const yourToken = await ethers.getContract("YourToken", deployer);
 
   // Todo: deploy the vendor
-  // await deploy("Vendor", {
-  //   from: deployer,
-  //   args: [yourToken.address], // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-  //   log: true,
-  // });
-  //
-  // const vendor = await ethers.getContract("Vendor", deployer);
+await deploy("Vendor", {
+  from: deployer,
+     args: [yourToken.address], // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+  log: true,
+  });
+
+const vendor = await ethers.getContract("Vendor", deployer);
 
   // Todo: transfer the tokens to the vendor
-  // console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
-  //
-  // const transferTransaction = await yourToken.transfer(
-  //   vendor.address,
-  //   ethers.utils.parseEther("1000")
-  // );
+  console.log("\n :rosette:  Sending all 1000 tokens to the vendor...\n");
 
-  //console.log("\n    ✅ confirming...\n");
-  //await sleep(5000); // wait 5 seconds for transaction to propagate
+  const transferTransaction = await yourToken.transfer(
+    vendor.address,
+    ethers.utils.parseEther("1000")
+  );
+
+  console.log("\n    :white_check_mark: confirming...\n");
+  await sleep(5000); // wait 5 seconds for transaction to propagate
 
   // ToDo: change address to your frontend address vvvv
-  // console.log("\n 🤹  Sending ownership to frontend address...\n")
-  // const ownershipTransaction = await vendor.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  // console.log("\n    ✅ confirming...\n");
-  // const ownershipResult = await ownershipTransaction.wait();
+  console.log("\n :juggling:  Sending ownership to frontend address...\n")
+  const ownershipTransaction = await vendor.transferOwnership("0xA91fb80b72Dd4f70d05EcDDC0F8b5200A9EB0035");
+  console.log("\n    :white_check_mark: confirming...\n");
+  const ownershipResult = await ownershipTransaction.wait();
 
   // ToDo: Verify your contract with Etherscan for public chains
   // if (chainId !== "31337") {
   //   try {
-  //     console.log(" 🎫 Verifing Contract on Etherscan... ");
+  //     console.log(" :ticket: Verifing Contract on Etherscan... ");
   //     await sleep(5000); // wait 5 seconds for deployment to propagate
   //     await run("verify:verify", {
   //       address: vendor.address,
@@ -47,7 +47,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   //       contractArguments: [yourToken.address],
   //     });
   //   } catch (e) {
-  //     console.log(" ⚠️ Failed to verify contract on Etherscan ");
+  //     console.log(" :warning: Failed to verify contract on Etherscan ");
   //   }
   // }
 };
